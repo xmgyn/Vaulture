c838d17bd5c815c2183deff17869aa1f7a5ee456
Mrigayan <connect.mrigayan@gmail.com>
2025-05-01 20:29:04 +0530

    Changes

--- Web App/src/main.jsx
+++ Web App/src/main.jsx
@@ -3,9 +3,34 @@ import { createRoot } from 'react-dom/client'
 
 import './main.css'
 
+const filetypes = {
+  folder: "/open-folder.png",
+  pdf: "/pdf.png",
+  video: "/video.png"
+}
+
+const Loading = () => {
+  return (
+    <div className="loading-bar"></div>
+  )
+}
+
+const Item = ({ type, name}) => {
+  return (
+    <div className="item">
+      <div className="item-logo">
+        <img src={ filetypes[type] }/>
+      </div>
+      <div className="item-title sono-500">{ name }</div>
+    </div>
+  )
+}
+
+
 function Main() {
   return (
     <>
+      <Loading />
       <div className="background">
         <img src="/Background.jpg" alt="" />
       </div>
@@ -16,11 +41,18 @@ function Main() {
           <div className="search"></div>
         </div>
         <div className="divider"></div>
-        <div className="repo"></div>
+        <div className="repo">
+          <Item type='folder' name='Wallpapers'/>
+
+        </div>
       </div>
       <div className="preview"></div>
+      <div className="timer">
+        <div className="timer-head sono-800">Time Remaining</div>
+        <div className="timer-text sono-400">07:46:02</div>
+      </div>
       <div className="controller">
-        {/* Sort, Show Hidden Files, Search, Open, Go Back, Details*/}
+        {/* Sort, Show Hidden Files, Download, Go Back, Details*/}
         {/* https://www.svgrepo.com/collection/xnix-circular-interface-icons/6
         https://www.svgrepo.com/svg/520534/backspace
         https://www.svgrepo.com/svg/520537/backword
